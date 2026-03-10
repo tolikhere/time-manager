3d1693cb82ab44e17b37bd31d436c0d419bfb8d6
Anatoliy <fenixdurangoadrenalin@gmail.com>
2026-03-10 20:22:04 +0300

    Remove the pause button. Instead of it now I'm using the start button to pause and start the timer. And also add svg icons instead of the text

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const title = document.getElementById("mode-title");
 const display = document.getElementById("display");
 const start = document.getElementById("start");
-const pause = document.getElementById("pause");
 const reset = document.getElementById("reset");
 const clear = document.getElementById("clear");
 const progressBar = document.getElementById("bar");
@@ -17,10 +16,15 @@ const shortBreak = MINUTE * 5;
 const longBreak = MINUTE * 20;
 const pulseClass = "pulse-warning";
 
+const PLAY_ICON = `<svg xmlns="http://www.w3.org" width="50" height="50" viewBox="0 0 24 24" fill="currentColor"><path d="M8 5v14l11-7z"/></svg>`;
+const PAUSE_ICON = `<svg xmlns="http://www.w3.org" width="50" height="50" viewBox="0 0 24 24" fill="currentColor"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>`;
+
+
 const sound = new Audio("./assets/audio/alarm.mp3");
 const audioCtx = new window.AudioContext();
 
 let intervalId = null;
+let isPaused = true;
 let tickToggle = true;
 let isWorkPhase = true;
 let timeLeft = workTime
@@ -96,6 +100,8 @@ const resetTimer = () => {
     ? "💻 Working hours" 
     : `${completedSessions === SESSIONS ? "🕹️ Long" : "☕ Short"} Break`;
   title.style.color = isWorkPhase ? "#f44336" : "#4caf50";
+  isPaused = true;
+  start.innerHTML = PLAY_ICON;
 };
 
 const updateProgressBar = () => {
@@ -169,18 +175,22 @@ display.textContent = formatTime(timeLeft);
 displaySessions();
 
 start.addEventListener("click", () => {
-  requestNotificationPermission(); // We ask once at the first launch
-  if (intervalId) return;
-  sound.pause();
-  sound.currentTime = 0;
-  start.disabled = true;
-  intervalId = setInterval(updateUI, 1000);
-});
+  if (isPaused) {
+    isPaused = false;
+    requestNotificationPermission(); // We ask once at the first launch
+    if (intervalId) return;
+    sound.pause();
+    sound.currentTime = 0;
+    // start.disabled = true;
+    intervalId = setInterval(updateUI, 1000);
+  } else {
+    clearInterval(intervalId);
+    intervalId = null;
+    // start.disabled = false;
+    isPaused = true
+  }
 
-pause.addEventListener("click", () => {
-  clearInterval(intervalId);
-  intervalId = null;
-  start.disabled = false;
+  start.innerHTML = isPaused ? PLAY_ICON : PAUSE_ICON;
 });
 
 reset.addEventListener("click", () => {
